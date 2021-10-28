9fb90e985cd64d7c7ea0b7ee06c8be15d7ec53f1
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2021-10-28 12:42:52 -0200

    Update Menu

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "325b562721d878db"; }
+/******/ 		__webpack_require__.h = function() { return "877e0369921b1584"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
