36e342c1b0729c3f304378880c634e16dc939321
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2021-10-29 01:31:09 -0200

    Botão

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "293c5c03e7f9684c"; }
+/******/ 		__webpack_require__.h = function() { return "6b705150e2ca7eb2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
